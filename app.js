16f7ce2855441fc4b391e8841ee52a8fd1c4d983
Liz A <lizethariasc14@gmail.com>
2022-06-18 12:25:30 -0500

    finished movefrog function

--- app.js
+++ app.js
@@ -10,21 +10,20 @@ function moveFrog(e) {
     squares[currenIndex].classList.remove('frog')
     switch(e.key) {
         case 'ArrowLeft':
-        console.log('move left')
-        currenIndex -= 1 
-        break;
+            if (currenIndex % width !== 0) currenIndex -= 1
+        break
         case 'ArrowRight':
-        console.log('move Right')
-        currenIndex += 1
-        break;
+        
+           if (currenIndex % width < width - 1 ) currenIndex += 1
+        break
         case 'ArrowUp':
-        console.log('move up')
-        currenIndex -= width
-        break;
+        
+           if (currenIndex - width >= 0) currenIndex -= width
+        break
         case 'ArrowDown':
-        console.log('move down')
-        currenIndex += width
-        break;
+       
+        if (currenIndex + width < width * width) currenIndex += width
+        break
     }
     
     squares[currenIndex].classList.add('frog')
