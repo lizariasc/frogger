95953829f7877507ebcdeccbbb5c5f48e72d1c27
Liz A <lizethariasc14@gmail.com>
2022-06-18 20:31:50 -0500

    cars movement

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const resultDisplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
 const logsLeft = document.querySelectorAll('.log-left');
+const logsRight = document.querySelectorAll('.log-right');
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
 
 let currenIndex = 76;
 const width = 9;
@@ -32,33 +35,97 @@ function moveFrog(e) {
 document.addEventListener('keyup', moveFrog)
 
 
-function autoMoveLogs() {
-    logsLeft.forEach(LogLeft => moveLogLeft(LogLeft))
+function autoMoveElements() {
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    logsRight.forEach(logRight => moveLogLeft(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRigth(carRight))
 }
 
-function moveLogLeft(LogLeft) {
+function moveLogLeft(logLeft) {
     switch (true) {
-      case LogLeft.classList.contains('l1'):
-          LogLeft.classList.remove('l1')
-          LogLeft.classList.add('l2')
+      case logLeft.classList.contains('l1'):
+           logLeft.classList.remove('l1')
+           logLeft.classList.add('l2')
           break
-      case LogLeft.classList.contains('l2'):
-          LogLeft.classList.remove('l2')
-          LogLeft.classList.add('l3')
+      case logLeft.classList.contains('l2'):
+          logLeft.classList.remove('l2')
+          logLeft.classList.add('l3')
           break
-       case LogLeft.classList.contains('l3'):
-            LogLeft.classList.remove('l3')
-            LogLeft.classList.add('l4')
+       case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
             break
-       case LogLeft.classList.contains('l4'):
-          LogLeft.classList.remove('l4')
-          LogLeft.classList.add('l5')
+       case logLeft.classList.contains('l4'):
+          logLeft.classList.remove('l4')
+          logLeft.classList.add('l5')
           break
-       case LogLeft.classList.contains('l5'):
-            LogLeft.classList.remove('l5')
-            LogLeft.classList.add('l1')
+       case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
             break
     }
 }
 
-setInterval(autoMoveLogs, 1000)
+
+function moveLogRight(logRight) {
+    switch (true) {
+        case logRight.classList.contains('l1'):
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+        case logRight.classList.contains('l2'):
+             logRight.classList.remove('l2')
+             logRight.classList.add('l1')
+            break
+         case logRight.classList.contains('l3'):
+              logRight.classList.remove('l3')
+              logRight.classList.add('l2')
+              break
+         case logRight.classList.contains('l4'):
+              logRight.classList.remove('l4')
+              logRight.classList.add('l3')
+            break
+         case logRight.classList.contains('l5'):
+              logRight.classList.remove('l5')
+              logRight.classList.add('l4')
+              break
+      }
+  }
+  
+  function moveCarLeft(carLeft) {
+    switch (true) {
+      case carLeft.classList.contains('c1'):
+        carLeft.classList.remove('c1')
+        carLeft.classList.add('c2')
+          break
+      case carLeft.classList.contains('c2'):
+        carLeft.classList.remove('c2')
+        carLeft.classList.add('c3')
+          break
+       case carLeft.classList.contains('c3'):
+        carLeft.classList.remove('c3')
+        carLeft.classList.add('c1')
+            break
+    }
+}
+
+function moveCarRigth(carRight) {
+    switch (true) {
+      case carRight.classList.contains('c1'):
+        carRight.classList.remove('c1')
+        carRight.classList.add('c3')
+          break
+      case carRight.classList.contains('c2'):
+        carRight.classList.remove('c2')
+        carRight.classList.add('c1')
+          break
+       case carRight.classList.contains('c3'):
+        carRight.classList.remove('c3')
+        carRight.classList.add('c2')
+            break
+    }
+}
+
+
+setInterval(autoMoveElements, 1000)
