f7a5ea8438af69c73495e39b00f6b4cd6ee8b52f
Liz A <lizethariasc14@gmail.com>
2022-06-18 20:07:30 -0500

    logs left movement

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
+const logsLeft = document.querySelectorAll('.log-left');
 
 let currenIndex = 76;
 const width = 9;
@@ -29,3 +30,35 @@ function moveFrog(e) {
     squares[currenIndex].classList.add('frog')
 }
 document.addEventListener('keyup', moveFrog)
+
+
+function autoMoveLogs() {
+    logsLeft.forEach(LogLeft => moveLogLeft(LogLeft))
+}
+
+function moveLogLeft(LogLeft) {
+    switch (true) {
+      case LogLeft.classList.contains('l1'):
+          LogLeft.classList.remove('l1')
+          LogLeft.classList.add('l2')
+          break
+      case LogLeft.classList.contains('l2'):
+          LogLeft.classList.remove('l2')
+          LogLeft.classList.add('l3')
+          break
+       case LogLeft.classList.contains('l3'):
+            LogLeft.classList.remove('l3')
+            LogLeft.classList.add('l4')
+            break
+       case LogLeft.classList.contains('l4'):
+          LogLeft.classList.remove('l4')
+          LogLeft.classList.add('l5')
+          break
+       case LogLeft.classList.contains('l5'):
+            LogLeft.classList.remove('l5')
+            LogLeft.classList.add('l1')
+            break
+    }
+}
+
+setInterval(autoMoveLogs, 1000)
